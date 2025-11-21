d1babe3295d15f3a25b232d67cadc55ff747de67
Brandon-2025 <er21008@ues.edu.sv>
2025-11-21 14:21:02 -0600

    corregido nombre de GenerarReporte.jsp

--- Sistema de gestion de atencion al cliente WEB/src/java/base_datos/ConexionBD.java
+++ Sistema de gestion de atencion al cliente WEB/src/java/base_datos/ConexionBD.java
@@ -11,7 +11,7 @@ import java.sql.SQLException;
 public class ConexionBD {
     private static final String URL ="jdbc:postgresql://localhost:5432/proyecto_prnIII";
     private static final String USER="postgres";
-    private static final String PASSWORD="Admin";
+    private static final String PASSWORD="admin";
     
     public static Connection conectar(){
         Connection conn=null;
